8972edd06faf566dea1887e4e8efac5b1dfd40dd
anastasiakrouglova <anastasia.krouglova@student.howest.be>
2019-12-16 12:00:25 +0100

    poging twee om get /... te fixen

--- server/index.js
+++ server/index.js
@@ -74,4 +74,12 @@ app.use('/api/heartRates', (req, res, next) => {
 });
 
 
-app.get('/*', (req, res) => res.send('Index Page'));
+// app.get('/*', (req, res) => res.send('Index Page'));
+
+app.get('/*', function(req, res) {
+    res.sendFile(path.join(__dirname, 'dist/index.html'), function(err) {
+      if (err) {
+        res.status(500).send(err)
+      }
+    })
+  })
